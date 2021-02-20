035f7dcc3eb2e204645e8fba0d4e76b8d4f8ca3e
Abdias Labrador LAptop windows10 asus <abdiaslabrador@gmail.com>
2021-02-20 00:37:51 -0400

    menu example and put some thoughts away

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Welcome my friend! House monitor")
+
+def menu():
+    print("House areas")
+    print("1-Kickchen")
+    print("2-Bathroom")
+    print("3-courtyard")
+    print("4-selftcare")
+    print("5-Foods")
+    print("6-Goals")    
+
+while int(input("Escriba una opción: ")) != -1:
+    menu()
+
